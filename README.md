1cbbf78e98179b8b42a6391da9bfb0616026b1e0
Henrique Melo <henrimelo10@gmail.com>
2020-10-23 23:46:26 -0300

    Allows for plotting of animation.

---
+++ README.md
@@ -0,0 +1,10 @@
+Requires FFmpeg. Install it with:
+```shell
+sudo apt update
+sudo apt install ffmpeg
+```
+
+Install all Python packages (preferably in a [virtual environment](https://docs.python.org/3/tutorial/venv.html)) with:
+```shell
+pip install -r requirements.txt
+```
--- plotter.py
+++ plotter.py
@@ -1,14 +1,14 @@
+from functools import partial
+import argparse
 import json
-import serial
 
 from matplotlib import animation
 import matplotlib.pyplot as plt
 import numpy as np
+import serial
 
 
-def create_fig():
-    with open('nodes_coords.json') as file:
-        nodes_coords = np.array(json.load(file)['nodes_coords'])
+def create_fig(nodes_coords):
     for i in range(len(nodes_coords)):
         plt.plot(nodes_coords[i][0], nodes_coords[i][1], 'ro', markersize=18)
         plt.annotate(f'{i}', (nodes_coords[i][0], nodes_coords[i][1]))
@@ -16,8 +16,14 @@ def create_fig():
     plt.grid()
     plt.xlabel('$x$ (m)')
     plt.ylabel('$y$ (m)')
-    xlim = 1.1 * np.array(np.min(nodes_coords[:,0]), np.max(nodes_coords[:,0]))
-    ylim = 1.1 * np.array(np.min(nodes_coords[:,1]), np.max(nodes_coords[:,1]))
+    xlim = np.array(
+        np.min(nodes_coords[:,0]) - .1*np.min(nodes_coords[:,0]),
+        np.max(nodes_coords[:,0]) + .1*np.max(nodes_coords[:,0])
+    )
+    ylim = np.array(
+        np.min(nodes_coords[:,1]) - .1*np.min(nodes_coords[:,1]),
+        np.max(nodes_coords[:,1]) + .1*np.max(nodes_coords[:,1])
+    )
     plt.xlim(xlim)
     plt.ylim(ylim)
 
@@ -37,7 +43,7 @@ def parse_serial():
     return source_coords
 
 
-def animate(nodes_coords):
+def animate(_):
     if len(fig.axes[0].lines) > 4:
         fig.axes[0].lines.pop(-1)
     source_coords = parse_serial()
@@ -45,18 +51,43 @@ def animate(nodes_coords):
 
 
 def main():
-    ani = animation.FuncAnimation(fig, animate, init_func=create_fig)
+    parser = argparse.ArgumentParser(description='Plot WASN localization.')
+    parser.add_argument('--nodes_coords', default='nodes_coords.json',
+                        type=str, nargs='?',
+                        help='path to JSON containing nodes coords.')
+    parser.add_argument('--port', default='/dev/ttyACM0',
+                        type=str, nargs='?',
+                        help='port to which Arduino is connected.')
+    parser.add_argument('--baudrate', default=115200, type=int, nargs='?',
+                        help='baudrate used in communication')
+    parser.add_argument('--timeout', default=100, type=int, nargs='?',
+                        help='timeout used in readlines')
+    parser.add_argument('--video', type=str, nargs='?',
+                        help='path to output video of figure')
+
+    args = parser.parse_args()
+    global ser
+    ser = serial.Serial()
+    ser.port = args.port
+    ser.baudrate = args.baudrate
+    ser.timeout = args.timeout
+    ser.open()
+    if ser.is_open==True:
+        print("\nSerial port now open. Configuration:\n")
+        print(ser, "\n")
+    global fig
+    fig = plt.figure(figsize=(8, 8))
+    with open('nodes_coords.json') as file:
+        nodes_coords = np.array(json.load(file)['nodes_coords'])
+    ani = animation.FuncAnimation(
+        fig, animate, init_func=partial(create_fig, nodes_coords)
+    )
+    if args.video:
+        Writer = animation.writers['ffmpeg']
+        writer = Writer(bitrate=1800)
+        ani.save(args.video, writer=writer)
     plt.show()
 
 
 if __name__ == '__main__':
-    ser = serial.Serial()
-    ser.port = '/dev/ttyACM1' #Arduino serial port
-    ser.baudrate = 115200
-    ser.timeout = 100 #specify timeout when using readline()
-    ser.open()
-    if ser.is_open==True:
-        print("\nAll right, serial port now open. Configuration:\n")
-        print(ser, "\n") #print serial parameters
-    fig = plt.figure(figsize=(8, 8))
     main()
